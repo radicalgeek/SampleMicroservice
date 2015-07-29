6e9dbe8a0effb657aa88bf402664ddf176ad53c8
Mark Jones <Mark.Jones2@experianinteractive.com>
2015-07-29 12:50:07 +0100

    adding new release build file

---
+++ debugbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export EnableNuGetPackageRestore=true
+cd .nuget
+test -e nuget.config || ln -s NuGet.Config nuget.config
+test -e nuget.exe || ln -s NuGet.exe nuget.exe
+test -e nuget.targets || ln -s NuGet.targets nuget.targets
+cd  ..
+nuget restore Prototype.sln
+xbuild /p:Configuration=Debug Prototype.sln
+mkdir -p output
+rm output/*
+cp -u */bin/Debug/* output
