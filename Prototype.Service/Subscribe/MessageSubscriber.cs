d67df6c0443dfcd8f4726a268299c86e300a5ae3
unknown <b06205a@W5105625.uk.experian.local>
2015-08-05 11:54:15 +0100

    Adding missing files

---
+++ Prototype.Service/Subscribe/MessageSubscriber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ;
+using EasyNetQ.Topology;
+using Prototype.Logger;
+using Prototype.Service.Consume;
+using Prototype.Service.Messages;
+using IEnvironment = Prototype.Service.Settings.IEnvironment;
+
+namespace Prototype.Service.Subscribe
+{
+    /// <summary>
+    /// Automaticly subscribes to messages, by locating consumers for message types
+    /// </summary>
+    public class MessageSubscriber : IMessageSubscriber
+    {
+        private readonly IAdvancedBus _bus;
+        private ILogger _logger;
+        private readonly IMessageConsumer _messageConsumer;
+        private IEnvironment _environment;
+        private IDisposable _consumer;
+        private readonly IExchange _exchange;
+        private readonly IQueue _queue;
+
+        public MessageSubscriber(IAdvancedBus bus,
+            IMessageConsumer messageConsumer, 
+            ILogger logger, 
+            IEnvironment environment,
+            IExchange exchange,
+            IQueue queue)
+        {
+            _messageConsumer = messageConsumer;
+            _bus = bus;
+            _logger = logger;
+            _environment = environment;
+            _exchange = exchange;
+            _queue = queue;
+        }
+
+        /// <summary>
+        /// Declares a new Exchange and Queue, and registers a consumer
+        /// </summary>
+        public void Start()
+        {     
+            _bus.Bind(_exchange, _queue, "A.*");
+             //_bus.Consume<object>(_queue, (message, info) => _messageConsumer.Consume((IMessage<SampleMessage>)message));
+
+             _consumer =  _bus.Consume<object>(_queue, (message, info) =>
+                Task.Factory.StartNew(() =>
+                    _messageConsumer.Consume(message)
+                    )
+                );
+        }
+
+        public void Stop()
+        {
+            _consumer.Dispose();
+        }
+    }
+}
---
+++ Prototype.Tests.Unit/MessageRoutingTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using EasyNetQ;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoRepository;
+using Moq;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Prototype.Logger;
+using NUnit;
+using Prototype.Service.Data;
+using Prototype.Service.Data.Model;
+using Prototype.Service.Filters;
+using Prototype.Service.Publish;
+using Prototype.Service.Routing;
+using Prototype.Service.Settings;
+using Prototype.Tests.Helpers;
+
+namespace Prototype.Tests.Unit
+{
+    /// <summary>
+    /// Summary description for SampleBusinessLogicTests
+    /// </summary>
+    [TestFixture]
+    public class MessageRoutingTests
+    {
+        [Test]
+        public void PostOperationCallsCreateDataOperation()
+        {
+            var env = new Mock<IEnvironment>();
+            var dataOps = new Mock<IDataOperations>();
+            var filter = new MessageFilter(env.Object);
+            var logicClass = new MessageRouter(env.Object, dataOps.Object, filter);
+            var message = TestMessages.GetTestCreateSampleEntityMessage();
+
+            logicClass.RouteSampleMessage(message);
+
+            dataOps.Verify(d => d.CreateSampleEntities(It.IsAny<object>()), Times.Once());
+        }
+
+        [Test]
+        public void GetOperationCallsGetDataOperation()
+        {    
+            var env = new Mock<IEnvironment>();
+            var dataOps = new Mock<IDataOperations>();
+            var filter = new MessageFilter(env.Object);
+            var logicClass = new MessageRouter(env.Object, dataOps.Object, filter);
+            var message = TestMessages.GetTestReadSampleEntityMessage();
+
+            logicClass.RouteSampleMessage(message);
+
+            dataOps.Verify(d => d.GetSampleEntities(It.IsAny<object>()),Times.Once());
+        }
+
+        [Test]
+        public void PutOperationCallsUpdateDataOperation()
+        {
+            var env = new Mock<IEnvironment>();
+            var dataOps = new Mock<IDataOperations>();
+            var filter = new MessageFilter(env.Object);
+            var logicClass = new MessageRouter(env.Object, dataOps.Object, filter);
+            var message = TestMessages.GetTestUpdateSampleEntityMesssage();
+
+            logicClass.RouteSampleMessage(message);
+
+            dataOps.Verify(d => d.UpdateSampleEntities(It.IsAny<object>()), Times.Once());
+        }
+
+       [Test]
+        public void DeleteOperationCallsDeleteDataOperation()
+        {
+            var env = new Mock<IEnvironment>();
+            var dataOps = new Mock<IDataOperations>();
+            var filter = new MessageFilter(env.Object);
+            var logicClass = new MessageRouter(env.Object, dataOps.Object, filter);
+            var message = TestMessages.GetTestDeleteSampleEntityMesssage();
+
+            logicClass.RouteSampleMessage(message);
+
+            dataOps.Verify(d => d.DeleteSampleEntities(It.IsAny<object>()), Times.Once());
+        }
+
+   
+
+    }
+
+ 
+
+
+}
